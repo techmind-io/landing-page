e3e23d5ab53daa42a56a463cb06394800dc3b076
Thibault Friedrich <tf@elqui.fr>
2018-01-18 00:11:53 +0100

    add indication to scroll + new preview for google+ + new background

--- src/front/scripts/index.js
+++ src/front/scripts/index.js
@@ -1,4 +1,116 @@
 $(document).ready(function () {
+  /*
+  particlesJS("techmind-background", {
+    "particles": {
+      "number": {
+        "value": 20,
+        "density": {
+          "enable": true,
+          "value_area": 500
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+    },
+    "retina_detect": true
+  });*/
+
   const controller = new ScrollMagic.Controller();
   new ScrollMagic.Scene({offset: -200, triggerElement: '#technologies', duration: 400})
   .on("progress", function (e) {
@@ -125,7 +237,7 @@ $(document).ready(function () {
     (function () {
       const draw = SVG('welcome-background');
       const group = draw.group();
-      const laptopContent = draw.image('./img/eye4.png', '100%', '100%').move(0, 0);
+      const laptopContent = draw.image('./img/eye5.png', '100%', '100%').move(0, 0);
       group.add(laptopContent);
 
       const radial = draw.gradient('radial', function (stop) {
@@ -303,6 +415,17 @@ $(document).ready(function () {
         $('#email').addClass('wrong');
       } */
   });
+
+  $('#continue').click(function () {
+    $('html, body').stop().animate({
+    'scrollTop': $('#technologies').offset().top
+      }, 2000, 'swing', function () {
+    });
+  });
+
+  setTimeout(function () {
+    $('#continue').removeClass('welcome-bottom-center-hide');
+  }, 1500);
 
 
 });
